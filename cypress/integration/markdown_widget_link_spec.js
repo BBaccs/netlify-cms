b6de87babaf3c632050035a6994f2231bf0d8d07
erezrokah <erezrokah@users.noreply.github.com>
2020-07-26 17:38:51 +0200

    fix(widget-markdown): properly check for selection when inserting links

---
+++ cypress/integration/markdown_widget_link_spec.js
@@ -0,0 +1,60 @@
+import '../utils/dismiss-local-backup';
+
+describe('Markdown widget link', () => {
+  before(() => {
+    Cypress.config('defaultCommandTimeout', 4000);
+    cy.task('setupBackend', { backend: 'test' });
+  });
+
+  beforeEach(() => {
+    cy.clearLocalStorage();
+    cy.reload();
+    cy.loginAndNewPost();
+    cy.clearMarkdownEditorContent();
+  });
+
+  after(() => {
+    cy.task('teardownBackend', { backend: 'test' });
+  });
+
+  describe('pressing backspace', () => {
+    it('can add a new valid link', () => {
+      const link = 'https://www.netlifycms.org/';
+      cy.window().then(win => {
+        cy.stub(win, 'prompt').returns(link);
+      });
+      cy.focused().clickLinkButton();
+      cy.confirmMarkdownEditorContent(`<p><a>${link}</a></p>`);
+      cy.clickModeToggle();
+      cy.confirmRawEditorContent(`<${link}>`);
+    });
+
+    it('can add a new invalid link', () => {
+      const link = 'www.netlifycms.org';
+      cy.window().then(win => {
+        cy.stub(win, 'prompt').returns(link);
+      });
+      cy.focused().clickLinkButton();
+      cy.confirmMarkdownEditorContent(`<p><a>${link}</a></p>`);
+      cy.clickModeToggle();
+      cy.confirmRawEditorContent(`[${link}](${link})`);
+    });
+
+    it('can select existing text as link', () => {
+      const link = 'https://www.netlifycms.org';
+      cy.window().then(win => {
+        cy.stub(win, 'prompt').returns(link);
+      });
+
+      const text = 'Netlify CMS';
+      cy.focused()
+        .getMarkdownEditor()
+        .type(text)
+        .setSelection(text)
+        .clickLinkButton();
+      cy.confirmMarkdownEditorContent(`<p><a>Netlify CMS</a></p>`);
+      cy.clickModeToggle();
+      cy.confirmRawEditorContent(`[${text}](${link})`);
+    });
+  });
+});
